1a2b9690d9b09e3c190f96722687e8787b15f1ff
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-11 21:09:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,15 @@
 # Fecha: 11 Septiembre 2024
 # Descripción: Código para procesamiento de imagenes con Sobel.
 # GitHub: https://github.com/Jona163
+
+from flask import Flask, render_template, request, redirect, url_for
+import cv2
+import numpy as np
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# Carpeta donde se almacenan las imágenes subidas y procesadas
+UPLOAD_FOLDER = 'static/img/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
